d3a7a7cdba8751c41b430d0dc2aad738b7138624
Christian Sunday <potentialsunny47@gmail.com>
2023-01-09 00:58:39 +0100

    created template and pass data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Patient
 
 # Create your views here.
+
+
+def index(request):
+    datas=Patient.objects.all()
+    return render(request, 'index.html', {'datas':datas})
