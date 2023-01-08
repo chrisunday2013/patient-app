7dda2530be50a54d6f40a6230b7bd39afbbd059f
Christian Sunday <potentialsunny47@gmail.com>
2023-01-08 22:54:55 +0100

    added patient model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Patient 
 
 # Register your models here.
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display=('full_name', 'mobile_no', 'detail', 'precaution', 'visit_date', 'next_visit_date')
+admin.site.register(Patient, PatientAdmin)
+
+
